98b0dbc16ab1b70080ac68f1542bfb4df3e1e31a
24tweikel <94697509+24tweikel@users.noreply.github.com>
2022-05-18 11:23:24 -0400

    stuff

---
+++ home.html
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html>
+
+    <head>
+
+        <title>English Project - Home</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" href="style.css">
+
+    </head>
+
+    <body>
+
+        <div class="topnav">
+
+            <a class="active" href="index.php">Home</a>
+            <a href="about.html">About</a>
+
+        </div>
+
+        <div style="padding-left:16px">
+
+            <h1>Hello, World!</h1>
+
+        </div>
+
+    </body>
+
+</html>
--- index.php
+++ index.php
@@ -3,7 +3,8 @@
 
     <head>
 
-        <title>English Project - Home</title>
+        <meta http-equiv="refresh" content="0; url=home.html"/>
+        <title>English Project - Redirect</title>
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <link rel="stylesheet" href="style.css">
 
@@ -11,18 +12,7 @@
 
     <body>
 
-        <div class="topnav">
-
-            <a class="active" href="index.php">Home</a>
-            <a href="about.html">About</a>
-
-        </div>
-
-        <div style="padding-left:16px">
-
-            <h1>Hello, World!</h1>
-
-        </div>
+        <p>REDIRECT</p>
 
     </body>
 
