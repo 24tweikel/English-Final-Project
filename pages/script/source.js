f8c3b93a33c51c4776d2d07feeaccd2303a2ecf9
24tweikel <94697509+24tweikel@users.noreply.github.com>
2022-05-23 20:17:00 -0400

    Changed More Stuff

--- pages/script/source.js
+++ pages/script/source.js
@@ -72,3 +72,78 @@ function Continue_pagetwo_click() {
     }
 
 }
+
+function Continue_pagethree_click() {
+
+    try {
+
+        window.location.href = 'pagefour.html';
+        
+    } catch (error) {
+
+        window.location.href = 'error.html';
+        document.getElementById("err").innerHTML = "Error: " + error;
+        
+    }
+
+}
+
+function Back_pagethree_click() {
+
+    try {
+
+        window.location.href = 'pagetwo.html';
+        
+    } catch (error) {
+
+        window.location.href = 'error.html';
+        document.getElementById("err").innerHTML = "Error: " + error;
+        
+    }
+
+}
+
+function Back_pagefour_click() {
+
+    try {
+
+        window.location.href = 'pagethree.html';
+        
+    } catch (error) {
+
+        window.location.href = 'error.html';
+        document.getElementById("err").innerHTML = "Error: " + error;
+        
+    }
+
+}
+
+function Continue_pagefour_click() {
+
+    try {
+
+        window.location.href = 'pagefive.html';
+        
+    } catch (error) {
+
+        window.location.href = 'error.html';
+        document.getElementById("err").innerHTML = "Error: " + error;
+        
+    }
+
+}
+
+function Back_pagefive_click() {
+
+    try {
+
+        window.location.href = 'pagefour.html';
+        
+    } catch (error) {
+
+        window.location.href = 'error.html';
+        document.getElementById("err").innerHTML = "Error: " + error;
+        
+    }
+
+}
